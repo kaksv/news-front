4164b7e7100e54bf1561a55c70334cd6c4c778d9
kaksv <kakoozavian@gmail.com>
2025-03-16 20:37:50 +0300

    c

--- src/http/httpClient.js
+++ src/http/httpClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    // baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://news-back-mcl1-i5xd1jkkf-kakooza-vianeys-projects.vercel.app',
     headers: {
         'Content-Type': 'application/json'
     }
